2c08d39941e94a3c57b4c7955f420a066e84f77d
TOTQWQ <168378974@qq.com>
2023-06-22 04:14:12 +0800

    更新

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,16 +1,3 @@
 <template>
     <h3>首页</h3>
 </template>
-<script setup>
-import myPromise from '../../myPromise'
-
-const promise = new myPromise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(1)
-    }, 1000)
-});
-
-promise.then()
-
-
-</script>
