f6a55b7985da4a987ee107d5f40b4aa625457f8e
TOTQWQ <168378974@qq.com>
2023-06-24 11:49:53 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', //解决打包访问静态资源路径问题
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js' //解决使用多语言警告问题
     }
   }
 })
